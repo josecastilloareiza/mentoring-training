9fd7b55f8f5f8c65929c66f8a02f096f88957abe
josecastilloareiza <josecastilloareiza@gmail.com>
2020-07-01 17:40:41 -0500

    Add basic unit test and http-server

---
+++ src/__tests__/sum.test.ts
@@ -0,0 +1,4 @@
+import { Sum } from '../index';
+test('My first test', () => {
+	expect(Sum(4,5)).toBe(9)
+})
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const tname = 'jose';
+
+export interface Menu {
+  name: string;
+}
+
+export function Sum(a: number, b: number): number {
+  return a + b;
+}
